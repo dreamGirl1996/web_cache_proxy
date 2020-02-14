b437273e0094b70d7128c4950d6b72e79d4ffff3
Chixiang Zhang <chixiang.zhang@duke.edu>
2020-02-14 18:41:23 -0500

    two directions

--- ProxyTest.h
+++ ProxyTest.h
@@ -12,57 +12,53 @@ bool testProxy(int argc, char *argv[]) {
     // Receive request from user's browser
     ServerSocket serverSocket;
     if (!serverSocket.setup()) {
-        std::cerr << "Error occured in setup\n";
+        std::cerr << "Error occured in server.setup\n";
         return false;
     }
     if (!serverSocket.socketRecv(recvFromUser)) {
-        std::cerr << "Error occured in socketRecv\n";
+        std::cerr << "Error occured in server.socketRecv\n";
         return false;
     }
     // Parse the request from the user
     std::string hostName;
     if (!parseObjectFromString(recvFromUser, hostName, parseHostNameFromALine)) {
-        std::cerr << "Error occured in parseObjectFromString\n";
+        std::cerr << "Error occured in server -parseObjectFromString\n";
         return false;
     }
     // End of parsing
 
     // Send user's request to the Web server, and receive the response from that Web server
-    // ClientSocket clientSocket(url);
-    // if (!clientSocket.setup()) {return false;}
-    // if (!clientSocket.socketSend(req)) {return false;}
-    // if (!clientSocket.socketRecv(recvMsg)) {return false;}
+    ClientSocket clientSocket(hostName);
+    if (!clientSocket.setup()) {
+        std::cerr << "Error occured in client.setup\n";
+        return false;
+    }
+    if (!clientSocket.socketSend(recvFromUser)) {
+        std::cerr << "Error occured in client.socketRecv\n";
+        return false;
+    }
+    if (!clientSocket.socketRecv(recvFromServer)) {
+        std::cerr << "Error occured in client -parseObjectFromString\n";
+        return false;
+    }
     // Parse the response from the Web server
     // End of parsing
 
     // Send Web server's response back to the user's browser
-    std::string sendMsg = "Hello World!";
-    if (!serverSocket.socketSend(sendMsg)) {return false;}
+    // std::string sendMsg = "Hello World!";
+    if (!serverSocket.socketSend(recvFromServer)) {return false;}
 
     printALine(32);
     std::cout << "Proxy server received:\n" << recvFromUser << "\n";
     printALine(32);
     std::cout << "hostName:\n" << hostName << "\n";
+    printALine(32);
+    std::cout << "Proxy client received:\n" << recvFromServer << "\n";
+
     serverSocket.closeSocket();
-    // clientSocket.closeSocket();
+    clientSocket.closeSocket();
 
     return true;
-
-    // std::string recvMsg;
-    // std::string sendMsg = "Hello World!!!";
-
-    // ServerSocket serverSocket;
-    // if (!serverSocket.setup()) {return false;}
-    // if (!serverSocket.socketRecv(recvMsg)) {return false;}
-    // if (!serverSocket.socketSend(sendMsg)) {return false;}
-    // serverSocket.closeSocket();
-
-    // printALine(32);
-    // std::cout << "Proxy server received:\n" << recvMsg << "\n";
-    // printALine(32);
-    // std::cout << "Proxy server sent:\n" << sendMsg << "\n";
-
-    // return true;
 }
 
 #endif
